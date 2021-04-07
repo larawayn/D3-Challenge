43e29376570a3c513f217d63d98143c0db657c36
larawayn <nlaraway10@gmail.com>
2021-04-06 18:37:21 -0600

    Update

--- app.js
+++ app.js
@@ -1,78 +1,7 @@
-// // @TODO: YOUR CODE HERE!
-// var svgWidth = 1000;
-// var svgHeight = 700;
-
-// // Define the chart's margins as an object
-// var chartMargin = {
-//     top: 30,
-//     right: 30,
-//     bottom: 30,
-//     left: 30
-//   };
-  
-//   // Define dimensions of the chart area
-//   var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-//   var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-
-// // create an SVG element
-// var svg = d3.select("#scatter")
-//   .append("svg")
-//   .attr("width", svgWidth)
-//   .attr("height", svgHeight);
-
-// d3.csv("data.csv").then(function(demographics) {
-
-//     console.log(demographics);
-
-//     var state = demographics.map(data => data.state);
-//     console.log("states", state);   
-
-//     var poverty = demographics.map(data => data.poverty);
-//     console.log("poverty", poverty)
-
-//     var healthcare = demographics.map(data => data.healthcare);
-//     console.log("healthcare", healthcare)
-
-
-
-//     var xScale = d3.scaleLinear()
-//                     .domain([8, 24])
-//                     .range([0, chartWidth]);
-//     svg.append("g")
-//         .attr("transform", "translate(0," + chartHeight + ")")
-//         .call(d3.axisBottom(xScale))
-
-
-
-//     var yScale = d3.scaleLinear()
-//                     .domain([4, 28] )
-//                     .range([chartHeight, 0]);
-//     svg.append("g")
-//         .call(d3.axisLeft(yScale));
-
-//   // Add dots
-//   svg.append('g')
-//     .selectAll("dot")
-//     .data(demographics)
-//     .enter()
-//     .append("circle")
-//       .attr("cx", function (d) { return xScale(d.poverty); } )
-//       .attr("cy", function (d) { return yScale(d.healthcare); } )
-//       .attr("r", 10)
-//       .style("fill", "#69b3a2")
-
-
-
-// }).catch(function(error) {
-//   console.log(error);
-
-
-// })
-
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 60, left: 60},
-    width = 700 - margin.left - margin.right,
-    height = 550 - margin.top - margin.bottom;
+    width = 1200 - margin.left - margin.right,
+    height = 900 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#scatter")
@@ -107,36 +36,29 @@ d3.csv("data.csv").then(function(data) {
 
     // Add dots
     svg.append('g')
-        .selectAll("circle")
+        .selectAll("dot")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function (d) { return x(d.poverty); } )
-        .attr("cy", function (d) { return y(d.healthcare); } )
-        .attr("r", 6)
+        .attr("cx", function (d) { return x(d['poverty']);})
+        .attr("cy", function (d) { return y(d['healthcare']);})
+        .attr("r", 16)
         .style("fill", "#69b3a2")
 
+    svg.append('g')
+        .selectAll('text')
+        .data(data)
+        .enter()
+        .append('text')
+        .text(d => d['abbr'])
+        .attr('x', (d) => x(d['poverty']))
+        .attr('y', (d) => y(d['healthcare']))
+        .attr('fontsize', '12px')
+        .style('font', 'bold Verdana')
+        .attr('text-anchor', 'middle')
+        .style('opacity', 0.85)
+        .style('fill', 'white')
 
-    var circleLabels = chartGroup
-                        .selectAll(null)
-                        .data(data)
-                        .enter()
-                        .append("text");
-
-    circleLabels
-            .attr("x", function(d) {
-                return x(d.poverty);
-            })
-            .attr("y", function(d) {
-                return y(d.healthcare);
-            })
-            .text(function(d) {
-                return d.state;
-            })
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "10px")
-            .attr("text-anchor", "middle")
-            .attr("fill", "white");
     
     // Create axes labels
     chartGroup.append("text")
